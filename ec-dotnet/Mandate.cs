5737711cb582ec8b89a9132b998ea29129ea0ffe
Sriduth Jayhari <sriduth.jayhari@juspay.in>
2019-09-13 10:57:38 +0530

    API / Mandate:
    * Added API for mandate txns
    * Added unit tests with partial coverage. [TODO]

--- ec-dotnet/Mandate.cs
+++ ec-dotnet/Mandate.cs
@@ -8,7 +8,7 @@ using Juspay.ExpressCheckout.Base;
 
 namespace Juspay.ExpressCheckout
 {
-    class Mandate
+    public class Mandate
     {
         public enum MandateMode
         {
@@ -16,10 +16,26 @@ namespace Juspay.ExpressCheckout
             OPTIONAL
         }
 
-        public static async Task<ECApiResponse> CreateMandateOrder(IDictionary<string, string> orderDetails, MandateMode mode, int maxAmount, ECApiCredentials creds = null)
+        private static readonly string[] MANDATE_REQ_PARAMETERS = { "order_id", "amount", "customer_id", "mandate_id"};
+
+        private static readonly string[] MANDATE_ALLOWED_PARAMETERS = {
+            "order_id", "amount", "currency", "customer_id", "mandate_id", "customer_email", "customer_phone", "product_id", "return_url", "description",
+
+            "billing_address_first_name", "billing_address_last_name", "billing_address_line1", "billing_address_line2", "billing_address_line3",
+            "billing_address_city", "billing_address_state", "billing_address_country","billing_address_postal_code","billing_address_phone",
+            "billing_address_country_code_iso",
+
+            "shipping_address_first_name","shipping_address_last_name","shipping_address_line1","shipping_address_line2","shipping_address_line3",
+            "shipping_address_city","shipping_address_state","shipping_address_postal_code", "shipping_address_phone","shipping_address_country_code_iso",
+            "shipping_address_country",
+
+            "udf1", "udf2", "udf3", "udf4", "udf5", "udf6", "udf7", "udf8", "udf9", "udf10",};
+        
+
+        public static async Task<ECApiResponse> CreateMandateOrder(IDictionary<string, string> orderDetails, MandateMode mode, string maxAmount, ECApiCredentials creds = null)
         {
             orderDetails.Add("options.create_mandate", mode.ToString());
-            orderDetails.Add("mandate_max_amount", maxAmount.ToString());
+            orderDetails.Add("mandate_max_amount", maxAmount);
 
             return await Orders.CreateOrder(orderDetails, creds);
         }
@@ -37,6 +53,54 @@ namespace Juspay.ExpressCheckout
         {
             return await HTTPUtils.ParseAndWrapResponseJObject(
                 await HTTPUtils.DoGet("/customers/" + customerId + "/mandates", null, creds));
+        }
+
+        /// <summary>
+        /// Execute a S2S call to perform a mandate transaction
+        /// </summary>
+        /// <param name="mandateOrderDetails">
+        ///     Dictionary holding the information of order to be created.
+        ///     Note that the "order." prefix is not required as the method
+        ///     adds it automatically.
+        ///     For a full list of parameters, refer:
+        ///     https://developer.juspay.in/reference#customer-initiates-the-transaction
+        /// </param>
+        /// <param name="mandateToken">
+        ///     Mandate token previously obtained.
+        /// </param>
+        /// <param name="creds">
+        ///     Optional API credentials to be passed which is chosen
+        ///     over the credentials configured using the 
+        ///     <see cref="Juspay.ExpressCheckout.Base.Config.Configure(Config.Environment, string, string, string)">
+        ///         Configure
+        ///     </see> API call.
+        /// </param>
+        /// <returns>ECApiResponse object having the JSON response data</returns>
+        /// <exception cref="ArgumentException">Thrown when parameters are not specified correctly.</exception>
+        public static async Task<ECApiResponse> ExecuteMandateTxn(
+            IDictionary<string, string> mandateOrderDetails,
+            string mandateToken,
+            ECApiCredentials creds = null)
+        {
+            foreach(string param in MANDATE_REQ_PARAMETERS)
+            {
+                if(!mandateOrderDetails.ContainsKey(param))
+                {
+                    throw new ArgumentException(param + " must be specified");
+                }
+            }
+
+            IDictionary<string, string> CleanDict = new Dictionary<string, string>();
+            foreach(string param in MANDATE_ALLOWED_PARAMETERS)
+            {
+                if (mandateOrderDetails.ContainsKey(param))
+                {
+                    CleanDict.Add("order." + param, mandateOrderDetails[param]);
+                }
+            }
+
+            return await HTTPUtils.ParseAndWrapResponseJObject(
+                await HTTPUtils.DoPost("/txns", CleanDict, creds));
         }
     }
 }
--- ec-dotnet/Orders.cs
+++ ec-dotnet/Orders.cs
@@ -48,9 +48,7 @@ namespace Juspay.ExpressCheckout
  
             "metadata.ZAAKPAY:productDescription", "metadata.ZAAKPAY:product1Description",
 
-            "metadata.PAYPAL:phone_number", "metadata.PAYPAL:first_name", "metadata.PAYPAL:last_name", "metadata.PAYPAL:country_code",
-
-            "options.create_mandate", "max_mandate_amount"
+            "metadata.PAYPAL:phone_number", "metadata.PAYPAL:first_name", "metadata.PAYPAL:last_name", "metadata.PAYPAL:country_code"
         };
 
         static Orders()
