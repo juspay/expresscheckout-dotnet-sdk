72756406966270158d5300c2f10d7436dcc90ebd
ramprakash.v@juspay.in <ramprakash.v@juspay.in>
2023-06-19 17:31:42 +0530

    Merchant Id added

--- Juspay/Services/_base/HttpClient.cs
+++ Juspay/Services/_base/HttpClient.cs
@@ -221,6 +221,7 @@ namespace Juspay
             }
         }
         private void AddRequestOptions(HttpRequestMessage request, JuspayRequest juspayRequest) {
+            request.Headers.Add("x-merchantid", JuspayEnvironment.MerchantId);
             if (juspayRequest.RequestOptions != null) {
                 RequestOptions requestOptions = juspayRequest.RequestOptions;
                 foreach (var x in FlattenObject(requestOptions)) {
